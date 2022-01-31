2bbf0626c6f695c348f4f8e022bd29b5c02b09ca
SOHTA0318 <nicosohta0318@gmail.com>
2022-01-31 14:11:46 -0800

    test & many association

--- sec 7 Many to Many, Test/app/controllers/categories_controller.rb
+++ sec 7 Many to Many, Test/app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+before_action :require_admin, except:[:index,:show]
+
   def new
   @category = Category.new
   end
@@ -11,7 +13,9 @@ class CategoriesController < ApplicationController
     render 'new'
   end
   end
-  def index;end
+  def index
+  @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
   def show
   @category = Category.find(params[:id])
   end
@@ -20,5 +24,12 @@ class CategoriesController < ApplicationController
 
   def category_params
 params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in && current_user.admin?)
+      flash[:alert] = 'Only admin can perform taht action'
+      redirect_to categories_path
+    end
   end
 end
