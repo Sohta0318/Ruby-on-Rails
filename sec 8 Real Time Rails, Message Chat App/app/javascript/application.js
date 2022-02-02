af7a002c1c6d8b5f66095fe4fc7cec68a3661b48
SOHTA0318 <nicosohta0318@gmail.com>
2022-02-02 14:39:48 -0800

    sec8

---
+++ sec 8 Real Time Rails, Message Chat App/app/javascript/application.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery3
+//= require semantic-ui
+
+import "@hotwired/turbo-rails";
+import "controllers";
+
+$(".ui.dropdown").dropdown();
