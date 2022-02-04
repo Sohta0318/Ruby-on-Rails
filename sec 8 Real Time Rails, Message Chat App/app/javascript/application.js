f465f3d3e69cef462702d4935bae4490c4537772
SOHTA0318 <nicosohta0318@gmail.com>
2022-02-03 19:45:10 -0800

    finish sec8

--- sec 8 Real Time Rails, Message Chat App/app/javascript/application.js
+++ sec 8 Real Time Rails, Message Chat App/app/javascript/application.js
@@ -4,5 +4,27 @@
 
 import "@hotwired/turbo-rails";
 import "controllers";
+import "channels";
+
+window.scroll_bottom = function () {
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  }
+};
+
+window.submit_message = function () {
+  $("#message_body").on("keydown", function (e) {
+    if (e.keyCode == 13) {
+      $("button").click();
+      e.target.value = "";
+    }
+  });
+};
 
 $(".ui.dropdown").dropdown();
+$(".message .close").on("click", function () {
+  console.log("hello");
+  $(this).closest(".message").transition("fade");
+});
+scroll_bottom();
+submit_message();
